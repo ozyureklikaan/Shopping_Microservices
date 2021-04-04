cc934a7d2a391e0924a148207e8f5094f87bc856
Kaan Ozyurekli <kaan.ozyurekli@d-teknoloji.com.tr>
2021-04-05 00:21:36 +0300

    Develop catalog microservice.

---
+++ src/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,90 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository productRepository, ILogger<CatalogController> logger)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _productRepository.GetProduct(id);
+
+            if (product == null)
+            {
+                _logger.LogError($"Product with id: {id}, not found.");
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("[action]/{name}")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByName(string name)
+        {
+            var products = await _productRepository.GetProductByName(name);
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("[action]/{categoryName}")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string categoryName)
+        {
+            var products = await _productRepository.GetProductByCategory(categoryName);
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> CrateProduct([FromBody]Product product)
+        {
+            await _productRepository.Crate(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProduct(Product product)
+        {
+            return Ok(await _productRepository.Update(product));
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            return Ok(await _productRepository.Delete(id));
+        }
+    }
+}
---
+++ src/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,82 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreConfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreConfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Iphone X",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+                    ImageFile = "product-1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Samsung S10",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+                    ImageFile = "product-2.png",
+                    Price = 840.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Huawei Plus",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+                    ImageFile = "product-3.png",
+                    Price = 950.00M,
+                    Category = "White Appliances"
+                },
+                new Product()
+                {
+                    Name = "Xiaomi Mi 9",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+                    ImageFile = "product-4.png",
+                    Price = 470.00M,
+                    Category = "White Appliances"
+                },
+                new Product()
+                {
+                    Name = "HTC U11+ Plus",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+                    ImageFile = "product-5.png",
+                    Price = 300.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "LG G7 ThinQ",
+                    Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+                    ImageFile = "product-6.png",
+                    Price = 240.00M,
+                    Category = "Home Kitchen"
+                },
+            };
+        }
+    }
+}
